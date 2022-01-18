bb3f13899c8784f9abc793c5ca896b46b0c41631
ian <ian@oneliveweb.com>
2022-01-17 21:30:35 -0500

    Added some sample field files.

---
+++ lib/Streaming/BenchStreamer.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/cupertino.dart';
+
+class BenchStreamer extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    return BenchStreamerState();
+    
+  }
+  
+}
+
+class BenchStreamerState extends State<BenchStreamer>{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
